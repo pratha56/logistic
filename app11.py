d131de18501cd8486310ebe7e76feaba3c925871
prathamesh sharad derore <77142866+pratha56@users.noreply.github.com>
2021-05-26 23:26:36 +0530

    Add files via upload

---
+++ app11.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 25 14:03:27 2021
+
+@author: asus
+"""
+
+import pandas as pd
+import streamlit as st 
+from sklearn.linear_model import LogisticRegression
+from pickle import dump
+from pickle import load
+
+st.title('Model Deployment: Logistic Regression')
+st.title("authour-prathamesh deore")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    CLMSEX = st.sidebar.selectbox('Gender',('1','0'))
+    CLMINSUR = st.sidebar.selectbox('Insurance',('1','0'))
+    SEATBELT = st.sidebar.selectbox('SeatBelt',('1','0'))
+    CLMAGE = st.sidebar.number_input("Insert the Age")
+    LOSS = st.sidebar.number_input("Insert Loss")
+    data = {'CLMSEX':CLMSEX,
+            'CLMINSUR':CLMINSUR,
+            'SEATBELT':SEATBELT,
+            'CLMAGE':CLMAGE,
+            'LOSS':LOSS}
+    features = pd.DataFrame(data,index = [0])
+    return features 
+    
+df = user_input_features()
+st.subheader('User Input parameters')
+st.write(df)
+
+
+# load the model from disk
+loaded_model = load(open('MyLogisticModel.sav', 'rb'))
+
+prediction = loaded_model.predict(df)
+prediction_proba = loaded_model.predict_proba(df)
+
+st.subheader('Predicted Result')
+st.write('Yes' if prediction_proba[0][1] > 0.5 else 'No')
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
+claimants=pd.read_csv(r'C:\Users\asus\Desktop\my desk\data science\logistic regression\claimants.csv')
+st.bar_chart(claimants)
+st.line_chart(claimants)
+st.color_picker('red')
